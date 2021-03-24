66e5094f22a220a81e20fa3bd71b3a43896a333a
Vladimir00505 <vladimir00505@gmail.com>
2021-03-24 19:05:14 +0300

    template created

---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+<nav className="App-nav">
+        <ul>
+          <li>
+            <a href="#s">Profile</a>
+          </li>
+          <li>
+            <a href="#s">Messages</a>
+          </li>
+          <li>
+            <a href="#s">News</a>
+          </li>
+          <li>
+            <a href="#s">Musisc</a>
+          </li>
+          <li>&nbsp;</li>
+          <li>
+            <a href="#s">Settings</a>
+          </li>
+        </ul>
+      </nav>
+    )
+}
+
+export default NavBar
---
+++ src/Components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="App-content">
+            <div className="content"></div>
+            <div>
+                <img className="ava" src="https://miro.medium.com/max/1838/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="avatar"></img>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Profile
